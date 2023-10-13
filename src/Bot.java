1eeb9e4165777a8fde97e95a9213d0ddf1f45522
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-10-13 20:12:17 +0700

    refactor : abstraction for base class

--- src/Bot.java
+++ src/Bot.java
@@ -1,6 +1,6 @@
-public class Bot {
-    public int[] move() {
-        // create random move
-        return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
+public abstract class Bot {
+    public abstract int[] move();
+    protected int getObjectiveFunc() {
+        return 0;
     }
 }
