8cccbac8574edcd59b5937eff29053f42ded41e1
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-10-17 22:30:37 +0700

    add: TreeNode

---
+++ src/bot/TreeNode.java
@@ -0,0 +1,48 @@
+package bot;
+
+import java.util.*;
+
+public class TreeNode {
+    Integer value;
+    Integer level;
+
+    boolean isMax;
+
+    Map<Integer, TreeNode> children;
+    TreeNode parent;
+
+    public boolean isLeaf = true;
+
+    public TreeNode(Integer value, Integer level, boolean isMax, boolean isLeaf) {
+        this.value = value;
+        this.level = level;
+        this.children = new HashMap<>();
+        this.parent = null;
+        this.isMax = isMax;
+        this.isLeaf = isLeaf;
+    }
+
+    public void addChildren(Integer index, TreeNode child) {
+        this.children.put(index, child);
+    }
+
+    public void updateValue(Integer value) {
+        if (this.value == null) {
+            this.value = value;
+            return;
+        }
+        if (this.isMax) {
+            this.value = Math.max(this.value, value);
+        } else {
+            this.value = Math.min(this.value, value);
+        }
+    }
+
+    public void updateValue(List<Integer> values) {
+        if (this.isMax) {
+            this.value = Collections.max(values);
+        } else {
+            this.value = Collections.min(values);
+        }
+    }
+}
